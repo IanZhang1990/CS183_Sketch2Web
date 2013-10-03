d6ff0c9a54c9688674fc355e85ed49313522a549
Yinan Zhang <ian.zhang1990@hotmail.com>
2013-10-03 15:09:22 -0400

    Upload Visual Studio 2010 Solution
    This solution is for visual studio 2010. If you are more comfortable
    with VS, you probably want to use this solution

--- Source/Sketch2Web/main.py
+++ Source/Sketch2Web/main.py
@@ -1,11 +1,11 @@
 #Test if your environment is setup
 
 import numpy
-import scipy
+#import scipy
 import cv2
 
 print numpy.__version__
-print scipy.__version__
+#print scipy.__version__
 print cv2.__version__
 
 print "Everything works!"
