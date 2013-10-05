092a276bfc1331c787d0066a1b24542124efa717
Yinan Zhang <ian.zhang1990@hotmail.com>
2013-10-04 22:09:55 -0500

    Perspective Correction 90% done.

--- Source/Sketch2Web/PerspectiveCorrection/QuadCorrector.py
+++ Source/Sketch2Web/PerspectiveCorrection/QuadCorrector.py
@@ -6,6 +6,7 @@ import sys
 import urllib2
 import cv2
 import numpy
+#import copy
 
 class QuadCorrector(object):
     """This class corrects quadrilateral objects' perspective view. 
@@ -15,8 +16,12 @@ class QuadCorrector(object):
         self.mCenter = ( 0, 0 );
         self.mInputImg = None;
         self.mOutputImg = None;
+        self.mOutputImgSize = ( 800, 600 )
         pass;
     
+    def setOutputSize( self, size ):
+        self.mOutputImgSize = size;
+
     def __showImg( self, img ):
         cv2.imshow( "Temp Window", img );
         cv2.waitKey(0)
@@ -58,13 +63,53 @@ class QuadCorrector(object):
             return [-1, -1]
         pass;
 
+    def classifyCorners(self, cornerPoints, center):
+        """Sort each point, and get the calculated topLeft/ topRight/ bottomLeft/ bottomRight point"""
+        topLeft = numpy.array([[0,0]]); topRight = numpy.array([[0,0]]);
+        bottomLeft = numpy.array([[0,0]]); bottomRight = numpy.array([[0,0]]);
+        # sort each point, find if they are topLeft/topRight/bottomLeft/bottomRight
+        for point in cornerPoints:
+            if point[1] < center[1] and point[0] < center[0]:
+                topLeft = numpy.append(topLeft,  [point], axis=0 )
+            elif point[1] < center[1] and point[0] > center[0]:
+                topRight = numpy.append( topRight, [point], axis=0 )
+            elif point[1] > center[1] and point[0] < center[0]:
+                bottomLeft = numpy.append( bottomLeft, [point], axis=0 )
+            elif point[1] > center[1] and point[0] > center[0]:
+                bottomRight = numpy.append( bottomRight, [point], axis=0 )
+                pass
+            pass
+
+        # for each point in each group, get the average value
+        tlAvg = [0,0]; trAvg = [0,0]; blAvg = [0,0]; brAvg = [0,0];
+        for point in topLeft:
+            tlAvg += point;
+        for point in topRight:
+            trAvg += point;
+        for point in bottomLeft:
+            blAvg += point;
+        for point in bottomRight:
+            brAvg += point;
+
+        if( len(topLeft) > 1 ):
+            tlAvg /= (len(topLeft)-1)
+        if( len(topRight) > 1 ):
+            trAvg /= (len(topRight)-1)
+        if( len(bottomLeft) > 1 ):
+            blAvg /= (len(bottomLeft)-1)
+        if( len(bottomRight) > 1 ):
+            brAvg /= (len(bottomRight)-1)
+
+        returnVal = numpy.float32([tlAvg, trAvg, blAvg, brAvg])
+        return returnVal
+        pass;
 
     def getOverViewImg( self, imageFilename ):
         self.mInputImg = cv2.imread( imageFilename )
         grayImg = cv2.cvtColor(self.mInputImg, cv2.COLOR_BGR2GRAY)
         blurImg = cv2.blur(grayImg, (3,3))
         edgesImg = cv2.Canny(blurImg, 100, 100, 3)
-        lines = cv2.HoughLines( edgesImg, 1, numpy.pi/180, 70,  30, 10 )
+        lines = cv2.HoughLines( edgesImg, 1, numpy.pi/180, 130 )
         vec4lines = []
         # Present the lines in a 4-element tuple
         for rho,theta in lines[0]:
@@ -77,7 +122,9 @@ class QuadCorrector(object):
             x2 = int(x0 - 1000*(-b))
             y2 = int(y0 - 1000*(a))
             vec4lines.append( [x1, y1, x2, y2] )
-            #cv2.line( self.mInputImg, (x1, y1), (x2,y2), (0, 0, 255), 1 )
+            #cv2.line( self.mInputImg, (x1, y1), (x2,y2), (255, 255, 255), 1 )
+
+        #sortedLines = self.sortLinesBySlop( vec4lines );
 
         # Get corners of the rectangle shape
         corners = None
@@ -86,19 +133,57 @@ class QuadCorrector(object):
                 intersectPoint = self.computeIntersection( vec4lines[i], vec4lines[j] )
                 if( intersectPoint[0] >= 0 and intersectPoint[1] >= 0 ):
                     if( corners is None ):
-                        corners = numpy.array([ [ intersectPoint[0], intersectPoint[0]] ])
+                        corners = numpy.array([ [ intersectPoint[0], intersectPoint[1]] ])
                     else:
                         corners = numpy.append( corners, [intersectPoint], axis=0 )
                     pass
+                #cv2.circle( self.mInputImg, (intersectPoint[0], intersectPoint[1]), 3, ( 255, 0, 0 ), 3)
                 pass
             pass
-
-        approx = cv2.approxPolyDP( corners, cv2.arcLength(corners, True ) * 0.025, True )
-        if( approx.size() != 4 ):
-            print "The object might not be quadrilateral!!\n"
-            return
+        self.showInput()
 
 
+
+        approx = cv2.approxPolyDP( corners, cv2.arcLength(corners, True ) * 0.02, True )
+        #if( len(approx) != 4 ):
+        #    print "The object might not be quadrilateral!!\n"
+        #    return
+
+        # Get mass center
+        center = [0,0]
+        for corner in corners:
+            center += corner
+
+        center /= len(corners)
+        # determin each corner point's position
+        quadCornerPoints = self.classifyCorners( corners, center )
+        
+        imgCopy = self.mInputImg.copy()
+        # Draw Lines based on the corner points we got
+        for i in range( 0, len( vec4lines ) ):
+            line = vec4lines[i]
+            cv2.line( imgCopy, (line[0], line[1]), (line[2], line[3]), (0, 255, 0), 1 )
+        #self.__showImg( imgCopy )
+
+        # Draw corner points
+        cv2.circle( imgCopy, (quadCornerPoints[0][0], quadCornerPoints[0][1]), 3, ( 255, 0, 0 ), 3)
+        cv2.circle( imgCopy, (quadCornerPoints[1][0], quadCornerPoints[1][1]), 3, ( 0, 255, 0 ), 3)
+        cv2.circle( imgCopy, (quadCornerPoints[2][0], quadCornerPoints[2][1]), 3, ( 0, 0, 255 ), 3)
+        cv2.circle( imgCopy, (quadCornerPoints[3][0], quadCornerPoints[3][1]), 3, ( 255, 255, 255 ), 3)
+
+        # Draw mass center 
+        cv2.circle( imgCopy, (center[0], center[1]), 3, ( 255, 255, 0 ), 2 )
+        self.__showImg( imgCopy )
+
+        imgCols = self.mOutputImgSize[0]
+        imgRows = self.mOutputImgSize[1] 
+        dstQuadPoints = numpy.float32( [[0,0], [imgCols,0], [0, imgRows], [imgCols, imgRows]] )
+        transMtrix = cv2.getPerspectiveTransform( quadCornerPoints, dstQuadPoints )
+        self.mOutputImg = cv2.warpPerspective( self.mInputImg, transMtrix, (imgCols,imgRows))
+        cv2.imshow( "Input", self.mInputImg );
+        cv2.imshow( "Output", self.mOutputImg );
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 x = numpy.array([[1,2]])
@@ -107,7 +192,9 @@ x= numpy.append( x, [[2,3]], axis=0 )
 #print numpy.append([[1, 2, 3]], [[7, 8, 9]], axis=0)
 
 corrector = QuadCorrector()
-corrector.getOverViewImg(".\Resources\\Poker.jpg")
+corrector.setOutputSize( (600, 800) )
+corrector.getOverViewImg(".\Resources\\best.jpg")
+#corrector.getOverViewImg(".\Resources\\Poker.jpg")
 
 
 
